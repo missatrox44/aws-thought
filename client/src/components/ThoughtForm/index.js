733995af2bf161c6e1c7767734a7c4b47e9d5238
Sara Baqla <missatrox44@gmail.com>
2023-04-21 14:51:40 -0500

    integrated image upload feature

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +6,9 @@ const ThoughtForm = () => {
     thought: "",
   });
   const [characterCount, setCharacterCount] = useState(0);
+
+  // initial value to null so that reference to the DOM element is current
+  const fileInput = useRef(null);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -21,11 +24,11 @@ const ThoughtForm = () => {
 
     // send request to API to create new thought
     const postData = async () => {
-      const res = await fetch('/api/users', {
-        method: 'POST',
+      const res = await fetch("/api/users", {
+        method: "POST",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formState),
       });
@@ -37,6 +40,31 @@ const ThoughtForm = () => {
     // clear form value
     setFormState({ username: "", thought: "" });
     setCharacterCount(0);
+  };
+
+  // function retrieves image file uploaded by user and sends data in request to endpoint
+  const handleImageUpload = (event) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('image', fileInput.current.files[0]);
+    // send image file to endpoint with the postImage function
+    const postImage = async () => {
+      try {
+        const res = await fetch('/api/image-upload', {
+          mode: 'cors',
+          method: 'POST',
+          body: data,
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        const postResponse = await res.json();
+        setFormState({ ...formState, image: postResponse.Location });
+    
+        return postResponse.Location;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postImage();
   };
 
   return (
@@ -62,6 +90,13 @@ const ThoughtForm = () => {
           className="form-input col-12 "
           onChange={handleChange}
         ></textarea>
+        <label className="form-input col-12  p-1">
+          Add an image to your thought:
+          <input type="file" ref={fileInput} className="form-input p-2" />
+          <button className="btn" onClick={handleImageUpload} type="submit">
+            Upload
+          </button>
+        </label>
         <button className="btn col-12 " type="submit">
           Submit
         </button>
@@ -71,3 +106,14 @@ const ThoughtForm = () => {
 };
 
 export default ThoughtForm;
+
+
+// IMAGE UPLOAD INPUT EXPLAINED
+// The <input> type is file. With this designation, HTML can browse and add files from your computer.
+
+// The ref attribute is assigned to the fileInput. We'll define this function using a React Hook name, useRef.
+
+// We'll also need to define the handleImageUpload function. This function will send the image to the image upload endpoint we created.
+
+// IMPORTANT
+// Because the image upload process to S3 is an asynchronous request that will take some time, it's possible to submit the form before the response from the image upload process is returned. This will submit a null as the value of the image URL. To prevent this, we can add a progress bar or disable the form submit button while the image is processing. However, because this is beyond the scope of this lesson, we will not attempt to add this feature.
